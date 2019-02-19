858dab4db5fbfd805baf8b657660e5f463f0f0a9
Aracki <aracki.ivan@gmail.com>
2019-02-19 17:14:43 +0100

    Add version command

--- cmd/root.go
+++ cmd/root.go
@@ -40,6 +40,15 @@ to quickly create a Cobra application.`,
 	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of cgc cli tool",
+	Long:  `All software has versions.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("v0.1")
+	},
+}
+
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
@@ -51,6 +60,7 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
+	rootCmd.AddCommand(versionCmd)
 
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
