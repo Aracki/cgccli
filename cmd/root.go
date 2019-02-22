8d2380190a810b686256e57d20bafbe91fd1a4fa
Aracki <aracki.ivan@gmail.com>
2019-02-22 10:55:13 +0100

    Refactor root.go

--- cmd/root.go
+++ cmd/root.go
@@ -16,28 +16,20 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/aracki/cgccli/cmd/files"
 	"os"
 
+	"github.com/aracki/cgccli/cmd/files"
 	"github.com/aracki/cgccli/cmd/projects"
 	"github.com/aracki/cgccli/cmd/version"
-	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "cgccli",
-	Short: "The CLI tool for the CGC Public API",
-	Long: `CLI tool should support following operations:
-		1. List projects 
-		2. List files in project
-		3. Get file details
-		4. Update file details
-		5. Download file`,
+	Short: "The Command Line Interface tool for the Cancer Genomics Cloud Public API",
+	Long:  `Use the cgccli to access the Platform via the API.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -50,47 +42,12 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
 	rootCmd.AddCommand(version.NewCmdVersion())
 	rootCmd.AddCommand(projects.NewCmdProjects())
 	rootCmd.AddCommand(files.NewCmdFiles())
 
+	// make --token PERSISTENT & GLOBAL flag and put it in the Viper registry
 	rootCmd.PersistentFlags().String("token", "", "Your authentication token that encodes your CGC credentials.")
 	rootCmd.MarkPersistentFlagRequired("token")
 	viper.BindPFlag("token", rootCmd.PersistentFlags().Lookup("token"))
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cgc.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".cgc" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".cgc")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
